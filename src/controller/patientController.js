4263fc59136e7ba51693596c74e6414dbb021f02
moybadajoz <moisesbadajoz36@gmail.com>
2024-05-16 20:37:22 -0600

    agregada base para modelo y crud de patient

--- src/controller/patientController.js
+++ src/controller/patientController.js
@@ -1,50 +1,6 @@
 const jwt = require('jsonwebtoken')
 const User = require('../models/User')
 
-
-const loginUser = async (req, res) => {
-    try {
-        const { email, password } = req.body
-
-        //Buscamos el usuario para verificar el correo electronico
-        //ahora con firebase-admin solo lo podemos poner asi
-        const userDoc = await User.findByEmail(email)
-
-        // Si no existe el usuario
-        if (!userDoc) {
-            return res.status(404).json({
-                message: 'User not found'
-            })
-        }
-
-        // verificar si el password es correcto
-        const isValidPass = await userDoc.verifyPassword(password)
-
-        if (!isValidPass) {
-            return res.status(401).json({
-                message: 'Invalid Credentials'
-            })
-        }
-
-        // General el token
-        const token = jwt.sign({ email: userDoc.email }, process.env.SECRET, { expiresIn: '1h' })
-        console.log(userDoc)
-        res.status(200).json({ 
-            message: 'success',
-            token,
-            user: {
-                email: userDoc.email,
-                name: userDoc.nombre
-            }
-        })
-
-    } catch {
-        res.status(500).json({
-            message: 'Internal Server Error'
-        })
-    }
-}
-
 const registerUser = async (req, res) => {
     try {
         const { email, password, nombre, apaterno, amaterno, direccion, telefono } = req.body
@@ -113,9 +69,5 @@ const updateUser = async (req, res) => {
 
 
 module.exports = { 
-    registerUser, 
-    loginUser, 
-    getAllUsers, 
-    deleteUser, 
-    updateUser 
+    registerPatient, 
 }
---
+++ src/interfaces/IPatient.js
@@ -0,0 +1,8 @@
+class IPatient {
+    /*
+        ~~~ Interface pacientes
+    */
+
+}
+
+module.exports = IPatient
